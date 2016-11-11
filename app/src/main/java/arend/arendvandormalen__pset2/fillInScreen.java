986d5209acf0c9ef53b122d842de19e2dcd8cbcf
ArendvD <arendvandormalen@gmail.com>
2016-11-11 15:00:53 +0100

    Displaying placeholder type as hint

--- app/src/main/java/arend/arendvandormalen__pset2/fillInScreen.java
+++ app/src/main/java/arend/arendvandormalen__pset2/fillInScreen.java
@@ -1,8 +1,16 @@
 package arend.arendvandormalen__pset2;
 
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Arend on 2016-11-08.
@@ -10,9 +18,65 @@ import android.support.v7.app.AppCompatActivity;
 
 public class fillInScreen extends AppCompatActivity{
 
+    Story story;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill_in_screen);
+        InputStream is;
+        try {
+            is = getAssets().open("madlib0_simple.txt");
+        } catch (IOException e){
+            e.printStackTrace();
+            is = null;
+        }
+
+        if (is != null) {
+            story = new Story(is);
+            setContentView(R.layout.activity_fill_in_screen);
+            int placeholderNum = story.getPlaceholderCount();
+            TextView textView = (TextView) findViewById(R.id.numLeft);
+            Log.d("placeholders left is", Integer.toString(placeholderNum));
+            textView.setText(Integer.toString(placeholderNum));
+            TextView hintView = (TextView) findViewById(R.id.fillInBox);
+            hintView.setHint(story.getNextPlaceholder());
+
+        }
+
     }
+
+    public void submitWord(View view){
+
+        if(story.isFilledIn()) {
+
+            Log.d("Story complete","ga naar volgend scherm");
+        } else{
+            // Select the text box
+            EditText contentEdit = (EditText) findViewById(R.id.fillInBox);
+
+            // Load input word
+            String word = contentEdit.getText().toString();
+            Log.d("chosen word is", word);
+            story.fillInPlaceholder(word);
+            contentEdit.setText("");
+
+            // update number of placeholders that is left
+            int placeholdersLeft = story.getPlaceholderRemainingCount();
+            TextView textView = (TextView) findViewById(R.id.numLeft);
+            Log.d("placeholders left is", Integer.toString(placeholdersLeft));
+            textView.setText(Integer.toString(placeholdersLeft));
+
+            // update displayed hint or placeholder type
+            contentEdit.setHint(story.getNextPlaceholder());
+        }
+
+
+
+
+    }
+
+
+
+
 }
